8cfa7a23ff3e49726bf5aee45ff390c73e52da77
Rene Meza <renemeza.escamilla@gmail.com>
2024-04-25 06:49:10 -0700

    fix: des eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["plugin:@next/next/recommended", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    // Desactivar reglas específicas para any
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-unsafe-member-access": "off",
-  },
-  ignorePatterns: ["*.js"],
-};
+// /** @type {import("eslint").Linter.Config} */
+// const config = {
+//   parser: "@typescript-eslint/parser",
+//   parserOptions: {
+//     project: true,
+//   },
+//   plugins: ["@typescript-eslint"],
+//   extends: ["plugin:@next/next/recommended", "plugin:@typescript-eslint/recommended"],
+//   rules: {
+//     // Desactivar reglas específicas para any
+//     "@typescript-eslint/no-explicit-any": "off",
+//     "@typescript-eslint/no-unsafe-assignment": "off",
+//     "@typescript-eslint/no-unsafe-call": "off",
+//     "@typescript-eslint/no-unsafe-member-access": "off",
+//   },
+//   ignorePatterns: ["*.js"],
+// };
 
-module.exports = config;
+// module.exports = config;
