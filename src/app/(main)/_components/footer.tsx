74499cd0b6a5503ba0d4e4f30d01f89e901c65f7
Raj <raj@d2america.com>
2024-08-31 19:56:28 +0530

    Make footer responsive

--- src/app/(main)/_components/footer.tsx
+++ src/app/(main)/_components/footer.tsx
@@ -37,24 +37,24 @@ import { PiPhoneCallDuotone } from "react-icons/pi";
 const Footer = () => {
     return (
         <>
-        <div className="p-28 pb-0 bg-black mt-28 text-white">
-            <div className="mb-9 mt-[-220px] w-full border border-gray-200 bg-gray-200 text-center shadow dark:border-0 dark:bg-gray-900 sm:p-8">
-                <h5 className="p-4 font-sans text-3xl text-gray-900 dark:text-white sm:text-4xl md:text-5xl lg:text-6xl">
-                    SUPPORT EVERY STEP OF THE WAY
-                </h5>
-                <p className="mb-5 text-sm text-[#8C8E8F] dark:text-gray-400 sm:text-base md:text-lg lg:text-xl">
-                    From placing an order to creating your perfect product,<br/> we're here to help every step of the way. Reach out anytime for support!
-                </p>
-            </div>
+            <div className="p-4 md:p-28 pb-0 bg-black mt-28 text-white">
+                <div className="mb-9 mt-[-90px] md:mt-[-150px] lg:mt-[-220px] w-full border border-gray-200 bg-gray-200 text-center shadow dark:border-0 dark:bg-gray-900 sm:p-8">
+                    <h5 className="p-4 font-sans text-xl text-gray-900 dark:text-white sm:text-2xl md:text-3xl lg:text-6xl">
+                        SUPPORT EVERY STEP OF THE WAY
+                    </h5>
+                    <p className="pb-4 mb-5 text-sm text-[#8C8E8F] dark:text-gray-400 sm:text-base md:text-lg lg:text-xl">
+                        From placing an order to creating your perfect product,<br /> we're here to help every step of the way. Reach out anytime for support!
+                    </p>
+                </div>
 
-            <div className="w-full py-4">
-            <div className="flex flex-col lg:flex-row items-center justify-between">
+                <div className="w-full py-4">
+                    <div className="flex flex-col lg:flex-row items-center justify-between">
                         <div className="flex items-center justify-center mb-4 lg:mb-0">
                             <Link href="/" className="">
                                 <Image src="/logo.png" width="80" height="80" alt="logo" />
                             </Link>
                         </div>
-                        <div className="flex flex-col lg:flex-row items-center justify-between gap-6 lg:h-12 lg:gap-0 lg:rounded-full lg:bg-gray-700 lg:p-0 mb-4 lg:mb-0">
+                        <div className="flex flex-col md:flex-row lg:flex-row items-center justify-between gap-6 lg:h-12 lg:gap-0 lg:rounded-full lg:bg-gray-700 lg:p-0 mb-4 lg:mb-0">
                             <input
                                 type="text"
                                 name="email"
@@ -68,95 +68,98 @@ const Footer = () => {
                                 Subscribe
                             </button>
                         </div>
-                        <div className="flex items-center justify-end mb-4 lg:mb-0">
-                            <a href="mailto:support@merchlife.com" className="rounded-full  flex items-center justify-center w-10 h-10 lg:w-12 lg:h-12 border-gray-400 border bg-gray-700">
-                                <IoMailOpenSharp className="w-4 h-4 lg:w-5 lg:h-5" />
-                            </a>
-                            <div className="space-y-0.5 font-medium dark:text-white text-left ms-3">
-                                <div>Email Us!</div>
-                                <a href="mailto:support@merchlife.com" className="text-sm text-gray-500 dark:text-gray-400 ">mailto:support@merchlife.com</a>
+                        <div className="md:flex">
+
+                            <div className="flex items-center justify-end mb-4 lg:mb-0 md:mr-3">
+                                <a href="mailto:support@merchlife.com" className="rounded-full  flex items-center justify-center w-10 h-10 lg:w-12 lg:h-12 border-gray-400 border bg-gray-700">
+                                    <IoMailOpenSharp className="w-4 h-4 lg:w-5 lg:h-5" />
+                                </a>
+                                <div className="space-y-0.5 font-medium dark:text-white text-left ms-3">
+                                    <div>Email Us!</div>
+                                    <a href="mailto:support@merchlife.com" className="text-sm text-gray-500 dark:text-gray-400 ">mailto:support@merchlife.com</a>
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex items-center justify-end mr-10">
-                            <a href="tel:+1 253 376 0307" className="rounded-full flex items-center justify-center w-10 h-10 lg:w-12 lg:h-12 border-gray-400 border bg-gray-700">
-                                <PiPhoneCallDuotone className="w-4 h-4 lg:w-5 lg:h-5" />
-                            </a>
-                            <div className="space-y-0.5 font-medium dark:text-white text-left ms-3">
-                                <div>Call Us!</div>
-                                <a href="tel:+1 253 376 0307" className="text-sm text-gray-500 dark:text-gray-400 ">+1 253 376 0307</a>
-                            </div>
-                        </div>                        
-                    </div>
-
-            </div>
-
-            <div className="w-full  border-b border-t border-gray-400">
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
-                    <div className="flex flex-col items-center justify-between gap-8 py-4 lg:flex-row ">
-                        <a href="https://pagedone.io/" className="flex justify-center "></a>
-                        <ul className="items-cente justify-center gap-14 text-center text-lg transition-all duration-500 sm:flex lg:gap-10 xl:gap-14">
-                            <li>
-                                <a href="/" className="text-white hover:text-gray-400">
-                                    Home
+                            <div className="flex items-center md:justify-end mr-10">
+                                <a href="tel:+1 253 376 0307" className="rounded-full flex items-center justify-center w-10 h-10 lg:w-12 lg:h-12 border-gray-400 border bg-gray-700">
+                                    <PiPhoneCallDuotone className="w-4 h-4 lg:w-5 lg:h-5" />
                                 </a>
-                            </li>
-                            <li className="my-2 sm:my-0">
-                                <a href="/stores" className="text-white hover:text-gray-400">
-                                    Stores
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/categories" className="text-white hover:text-gray-400">
-                                    Categories
-                                </a>
-                            </li>
-                            <li className="my-2 sm:my-0">
-                                <a href="/how-it-works" className="text-white hover:text-gray-400">
-                                    How it works
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/about" className="text-white hover:text-gray-400">
-                                    About
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/faq" className="text-white hover:text-gray-400">
-                                    FAQ
-                                </a>
-                            </li>
-                        </ul>
-                        <div className="flex  space-x-4 sm:justify-center  "></div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="w-full py-4">
-                <div className="mx-auto max-w-7xl">
-                    <div className="">
-                        <div className="flex flex-col items-center justify-center lg:flex-row lg:justify-between">
-                            <span className="text-sm text-gray-500 ">
-                                Copyright © 2024 Merchlife. All Rights Reserved.
-                            </span>
-                            <div className="mt-4 flex space-x-4 sm:justify-center lg:mt-0 ">
-                                <a
-                                    href="/terms-of-use"
-                                    className=" items-centerhover:bg-gray-900 flex justify-center duration-500"
-                                >
-                                    Terms of Service
-                                </a>
-
-                                <a
-                                    href="/privacy-policy"
-                                    className="relative flex items-center justify-center duration-500  hover:bg-gray-900 "
-                                >
-                                    Privacy Policy
-                                </a>
+                                <div className="space-y-0.5 font-medium dark:text-white text-left ms-3">
+                                    <div>Call Us!</div>
+                                    <a href="tel:+1 253 376 0307" className="text-sm text-gray-500 dark:text-gray-400 ">+1 253 376 0307</a>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
-            </div>
+
+                <div className="w-full  border-b border-t border-gray-400">
+                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
+                        <div className="flex flex-col items-center justify-between gap-8 py-4 lg:flex-row ">
+                            <a href="https://pagedone.io/" className="flex justify-center "></a>
+                            <ul className="items-cente justify-center gap-14 text-center text-lg transition-all duration-500 sm:flex sm:gap-8 md:gap-5 lg:gap-10 xl:gap-14">
+                                <li>
+                                    <a href="/" className="text-white hover:text-gray-400">
+                                        Home
+                                    </a>
+                                </li>
+                                <li className="my-2 sm:my-0">
+                                    <a href="/stores" className="text-white hover:text-gray-400">
+                                        Stores
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/categories" className="text-white hover:text-gray-400">
+                                        Categories
+                                    </a>
+                                </li>
+                                <li className="my-2 sm:my-0">
+                                    <a href="/how-it-works" className="text-white hover:text-gray-400">
+                                        How it works
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/about" className="text-white hover:text-gray-400">
+                                        About
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/faq" className="text-white hover:text-gray-400">
+                                        FAQ
+                                    </a>
+                                </li>
+                            </ul>
+                            <div className="flex  space-x-4 sm:justify-center  "></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-full py-4">
+                    <div className="mx-auto max-w-7xl">
+                        <div className="">
+                            <div className="flex flex-col items-center justify-center lg:flex-row lg:justify-between">
+                                <span className="text-sm text-gray-500 ">
+                                    Copyright © 2024 Merchlife. All Rights Reserved.
+                                </span>
+                                <div className="mt-4 flex space-x-4 sm:justify-center lg:mt-0 ">
+                                    <a
+                                        href="/terms-of-use"
+                                        className=" items-centerhover:bg-gray-900 flex justify-center duration-500"
+                                    >
+                                        Terms of Service
+                                    </a>
+
+                                    <a
+                                        href="/privacy-policy"
+                                        className="relative flex items-center justify-center duration-500  hover:bg-gray-900 "
+                                    >
+                                        Privacy Policy
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
